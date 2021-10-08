5bfe0a5440e6952fc790840c78b2a1aba3b6a6c7
AshwinRamachandran2002 <200050016@iitb.ac.in>
2021-10-08 15:48:56 +0530

    final

---
+++ algorithms/single_chain.py
@@ -0,0 +1,30 @@
+grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+N=9
+def isSafe(grid,row,col):
+    pass
+
+
+def solveSuduko(grid, row, col):
+    if (row == N - 1 and col == N):
+        return True
+    if col == N:
+        row += 1
+        col = 0
+    if grid[row][col] > 0:
+        return solveSuduko(grid, row, col + 1)
+    for num in range(1, N + 1, 1):
+        if isSafe(grid, row, col, num):
+            grid[row][col] = num
+            if solveSuduko(grid, row, col + 1):
+                return True
+        grid[row][col] = 0
+    return False
