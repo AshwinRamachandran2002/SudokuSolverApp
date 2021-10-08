b09426b8d0bc1ba3a001e11bf7d5c71395ad4413
AshwinRamachandran2002 <200050016@iitb.ac.in>
2021-10-08 15:48:01 +0530

    changes to styling and retrofit 2.0

--- sudokuSolverinjava/app/src/main/java/com/example/sudokusolverinjava/MainActivity.java
+++ sudokuSolverinjava/app/src/main/java/com/example/sudokusolverinjava/MainActivity.java
@@ -1,92 +1,241 @@
 package com.example.sudokusolverinjava;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import static java.security.AccessController.getContext;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.text.DecimalFormat;
+import java.util.Comparator;
+class ServiceGenerator {
+
+    private static final String BASE_URL = "https://api.github.com/";
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    private static OkHttpClient.Builder httpClient =
+            new OkHttpClient.Builder();
+
+    public static <S> S createService(
+            Class<S> serviceClass) {
+        return retrofit.create(serviceClass);
+    }
+}
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.sudokuSolverinjava.MESSAGE";
+    private String Document_img1="";
+    public static final String KEY_User_Document1 = "photo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int REQUEST_IMAGE_CAPTURE = 100;
         Button btnCapture = findViewById(R.id.cameraBtn);
         ImageView ivShowImage = findViewById(R.id.displayImageView);
         EnableRuntimePermission();
+        verifyStoragePermissions(this);
         btnCapture.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                startActivityForResult(intent, 7);
-
+               intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+               File dir=new File(android.os.Environment.getExternalStorageDirectory()+"/default");
+               dir.mkdirs();
+                File f = new File(android.os.Environment.getExternalStorageDirectory()+"/default", "temp.jpg");
+                if(f.exists()) f.delete();
+                try {
+                    f.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                startActivityForResult(intent, 1);
             }
         });
         Button solve = findViewById(R.id.solve);
-//        solve.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//
-//                Intent intent = new Intent(getBaseContext(), displayactivity.class);
-//                String message = "aa";
-//                intent.putExtra("bitmap", message);
-//                startActivity(intent);
-//
-//            }
-//        });
+    }
+    public String BitMapToString(Bitmap userImage1) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        userImage1.compress(Bitmap.CompressFormat.PNG, 60, baos);
+        byte[] b = baos.toByteArray();
+        Document_img1 = Base64.encodeToString(b, Base64.DEFAULT);
+        return Document_img1;
     }
 
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
 
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
     public static final int RequestPermissionCode  = 1 ;
-
-
-
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 7 && resultCode == RESULT_OK) {
-
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+           // Bitmap bitmap1 = (Bitmap) data.getExtras().get("data");
             ImageView ivShowImage = findViewById(R.id.displayImageView);
-            ivShowImage.setImageBitmap(bitmap);
+
+            File f = new File(Environment.getExternalStorageDirectory().toString()+"/default");
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+            try {
+                Bitmap bitmap;
+                BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                bitmap=getResizedBitmap(bitmap, 400);
+                ivShowImage.setImageBitmap(bitmap);
+                BitMapToString(bitmap);
+                String path = android.os.Environment
+                        .getExternalStorageDirectory()
+                        + File.separator
+                        + "default";
+                f.delete();
+                OutputStream outFile = null;
+                File file = new File(path, "okk.jpg");
+                file.createNewFile();
+//                try {
+                    outFile = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                    outFile.flush();
+                    outFile.close();
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             Button solve = findViewById(R.id.solve);
             solve.setVisibility(View.VISIBLE);
+            //File finalF = f;
             solve.setOnClickListener(new View.OnClickListener() {
-
                 @Override
                 public void onClick(View v) {
+                    try{
+                        uploadFile();
+                    }catch(Exception e){
+                        //Log.d(e.toString());
+                    }
 
                     Intent intent = new Intent(getBaseContext(), displayactivity.class);
-                    String message = "aa";
-                    intent.putExtra(EXTRA_MESSAGE, bitmap);
+                    String message = "123456789123456789123456789123456789123456789123456789123456789123456789123456789";
+                    intent.putExtra(EXTRA_MESSAGE, message);
                     startActivity(intent);
-
                 }
             });
         }
     }
-
     public void EnableRuntimePermission(){
-
         if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
                 Manifest.permission.CAMERA))
         {
@@ -122,5 +271,90 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+    public interface FileUploadService {
+        @Multipart
+        @POST("upload")
+        Call<ResponseBody> upload(
+                @Part("description") RequestBody description,
+                @Part MultipartBody.Part file
+        );
+    }
+    private void uploadFile() {
+        // create upload service client
+        FileUploadService service =
+                ServiceGenerator.createService(FileUploadService.class);
+
+        // https://github.com/iPaulPro/aFileChooser/blob/master/aFileChooser/src/com/ipaulpro/afilechooser/utils/FileUtils.java
+        // use the FileUtils to get the actual file by uri
+        File file=new File(String.valueOf(Environment.getExternalStorageDirectory())+ File.separator+"default"+File.separator+"temp.jpg");
+        // create RequestBody instance from file
+        RequestBody requestFile =
+                RequestBody.create(
+                        MediaType.parse("image/*"),
+                        file
+                );
+
+        // MultipartBody.Part is used to send also the actual file name
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("photo", file.getName(), requestFile);
+
+        // add another part within the multipart request
+        String descriptionString = "hello, this is description speaking";
+        RequestBody description =
+                RequestBody.create(
+                        okhttp3.MultipartBody.FORM, descriptionString);
+
+        // finally, execute the request
+        Call<ResponseBody> call = service.upload(description, body);
+        call.enqueue(new Callback<ResponseBody>() {
+
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+                Log.v("Upload", "success");
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("Upload error:", t.getMessage());
+            }
+        });
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, read);
+        }
+    }
 }
 
--- sudokuSolverinjava/app/src/main/java/com/example/sudokusolverinjava/displayactivity.java
+++ sudokuSolverinjava/app/src/main/java/com/example/sudokusolverinjava/displayactivity.java
@@ -5,65 +5,217 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class displayactivity extends AppCompatActivity {
+
+    // recieve the string from api here
     Intent intent;
-    Bitmap message;
+    String message;
+
+    // experimentation with tflite module
     DigitsDetector mnistClassifier;
     final int PIXEL_WIDTH = 28;
+
+
     private final String TAG = this.getClass().getSimpleName();
+    private TableLayout mTableLayout;
+    private Board mBoard;
+    private Button mClearBoardButton;
+    private Button mFullySolveButton;
+    private boolean ignoreNextText; //decides whether to skip textWatcher
+
+    final static int DEFAULT_BOARD_SIZE = 9;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_displayactivity);
-        Button back = findViewById(R.id.back);
-        Button change = findViewById(R.id.change);
+
+
         intent = getIntent();
         message = intent.getParcelableExtra(MainActivity.EXTRA_MESSAGE);
-        //mnistClassifier = new DigitsDetector(this);
-        //ImageView ivShowImage = findViewById(R.id.displayImageView);
-        //onDetectClicked();
-        change.setOnClickListener(new View.OnClickListener() {
+        mBoard = new Board(DEFAULT_BOARD_SIZE,message);
 
+        setContentView(R.layout.activity_puzzle_solver);
+        mTableLayout = (TableLayout) findViewById(R.id.board_table);
+        createTable(DEFAULT_BOARD_SIZE);
+
+        ignoreNextText = false;
+
+        mClearBoardButton = (Button) findViewById(R.id.clear_puzzle_button);
+        mClearBoardButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView[] res = new TextView[9];
-                for(int i = 1; i <= 9; i++) {
-                    String ida="digit"+i;
-                    int resID = getResources().getIdentifier(ida, "id", getPackageName());
-                    res[i] = ((TextView) findViewById(resID));
+                clearEditTexts();
+                mBoard.deleteData();
+                if (!mFullySolveButton.isEnabled())
+                    mFullySolveButton.setEnabled(true);
+            }
+        });
+
+        mFullySolveButton = (Button) findViewById(R.id.fully_solve_button);
+        mFullySolveButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBoard.GatherChanges();
+                if (mBoard.solvePuzzle()) {
+                    TableEntryEditText tempET;
+                    int size = mBoard.getSize();
+                    ignoreNextText = true;
+                    for (int i = 0; i < size; i++) {
+                        for (int j = 0; j < size; j++) {
+                            tempET = (TableEntryEditText) findViewById(i * size + j);
+                            if (tempET != null) {
+                                tempET.setText(String.format("%s", mBoard.getSolvedData(i * size + j)));
+                                tempET.disableEditability();
+                            }
+                        }
+                    }
+                    ignoreNextText = false;
+                    mFullySolveButton.setEnabled(false);
                 }
-//                CharSequence digit=res.getText();
-//                Log.d(TAG,digit.toString());
-//                String newdigit=digit.toString();
-//                if(newdigit=="6"){
-//
-//
-//                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Puzzle could not be solved", Toast.LENGTH_SHORT).show();
+                }
+
+                //InputMethodManager imm = (InputMethodManager) PuzzleSolver.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                //if (imm.isAcceptingText())
+                //imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0); //hide keyboard if its open - VERY BUGGY
             }
         });
-        back.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
+    }
 
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(intent);
-
+    private void clearEditTexts() {
+        ignoreNextText = true;
+        for (int i = 0; i < mTableLayout.getChildCount(); i++) { //loops through all rows in tableLayout
+            TableRow tempTR = (TableRow) mTableLayout.getChildAt(i); //sets a temp TableRow
+            for (int j = 0; j < tempTR.getChildCount(); j++) { //loops through all editTexts in current TableRow
+                TableEntryEditText et = (TableEntryEditText) tempTR.getChildAt(j);
+                et.enableEditability();
+                et.setText(""); //clears the editText
+                et.setInvalidEntry(false);
             }
-        });
+        }
+        ignoreNextText = false;
     }
 
 
-    private void onDetectClicked() {
-        TextView res = findViewById(R.id.digit1);
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(message, PIXEL_WIDTH, PIXEL_WIDTH, false);
-        int digit = mnistClassifier.classify(scaledBitmap);
-        res.setText(digit);
+    @Override
+    protected void onSaveInstanceState(Bundle SavedInstanceState) {
+        super.onSaveInstanceState(SavedInstanceState);
+
+        String boardData;
+    }
+
+    private void checkEntry(int editTextId) {
+        TableEntryEditText tempET;
+        int startPos;
+
+        switch (mBoard.checkData(editTextId)) {
+            case 1:
+                //set col background for elements to red
+                for (int i = mBoard.computeX(editTextId); i < mBoard.getSize() * mBoard.getSize(); i+=9) {
+                    tempET = (TableEntryEditText) findViewById(i);
+                    if (tempET != null)
+                        tempET.setInvalidEntry(true);
+                    //tempET.setBackgroundResource(R.drawable.invalid_cell);
+                }
+                break;
+            case 2:
+                //set row background for elements to red
+                startPos = mBoard.computeY(editTextId) * mBoard.getSize();
+                for (int i = startPos; i < startPos + 9; i++) {
+                    tempET = (TableEntryEditText) findViewById(i);
+                    if (tempET != null)
+                        tempET.setInvalidEntry(true);
+                    //tempET.setBackgroundResource(R.drawable.invalid_cell);
+                }
+                break;
+            case 3:
+                //set box background for elements to red
+                startPos = (editTextId - (editTextId % 3)) - ((editTextId / 9) % 3 * 9);
+                for (int y = startPos; y < 27 + startPos; y += 9) {
+                    for (int loc = y; loc < y + 3; loc++) {
+                        tempET = (TableEntryEditText) findViewById(loc);
+                        if (tempET != null)
+                            tempET.setInvalidEntry(true);
+                        //tempET.setBackgroundResource(R.drawable.invalid_cell);
+                    }
+                }
+
+                break;
+            case 0:
+                //remove element from list and set background to normal
+                break;
+        }
+    }
+
+    private void createTable(final int size,String initial_data) {
+        if(initial_data==""){
+            initial_data="000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+        }
+        for (int i = 0; i < size; i++) {
+            final TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams( new TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)); //sets width and height
+            tableRow.setId(i + 100); //sets id between 100 and 100 + boardSize
+
+            for (int j = 0; j < size; j++) {
+                final TableEntryEditText editText = new TableEntryEditText(this);
+                editText.setId(i * size + j); //sets id between 0 and 80
+
+                mTableLayout.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        editText.setHeight(mTableLayout.getWidth() / 9);
+                    }
+                });
+
+
+                editText.addTextChangedListener(new TextWatcher() {
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if(ignoreNextText)
+                            return;
+
+                        if (editText.length() > 1) {
+                            String newInput = s.toString().substring(s.length() - 1); //grabs the last value entered
+                            editText.setText(newInput); //sets the text to the newInput
+                            editText.setSelection(editText.length()); //sets cursor to end of editText
+                            checkEntry(editText.getId());
+                        }
+                        else if (editText.length() == 1 && s.length() == 1) {
+                            mBoard.setData(editText.getId(), Integer.parseInt(s.toString())); //sets board data when something entered
+                            checkEntry(editText.getId());
+                        }
+                        else if (count == 0 && editText.length() == 0) {
+                            mBoard.deleteSingleData(editText.getId());
+                            checkEntry(editText.getId());
+                            Log.v("onTextChanged", "deleting data at index " + editText.getId());
+                        }
+                    }
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                    @Override
+                    public void afterTextChanged(Editable s) {}
+                });
+                editText.setText(initial_data.charAt(i*size+j));
+                tableRow.addView(editText);
+            }
+
+            mTableLayout.addView(tableRow); //add the row to the table
+        }
     }
 
 }
